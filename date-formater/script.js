5453d93cb981911b10a8c27b43cc66e5c0f3e0f5
faqihfnf <faqih.fnf@gmail.com>
2025-01-06 12:08:22 +0700

    date-formater until step 17

--- date-formater/script.js
+++ date-formater/script.js
@@ -1,1 +1,19 @@
 const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+
+const day = date.getDate();
+const month = date.getMonth() + 1; // ditambah 1, karena secara default bulan dimulai dari 0 pada method getMonth()
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const formattedDate = `${day}-${month}-${year}`;
+formattedDate = currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {});
+
+switch (dateOptionsSelectElement.value) {
+  case "yyyy-mm-dd":
+    currentDateParagraph.textContent = formattedDate;
+}
