4d00dc3605d870061b0b0a5dd5d9caa8a8465d98
faqihfnf <faqih.fnf@gmail.com>
2025-01-09 19:26:35 +0700

    todo-app until step 60

--- todo-app/script.js
+++ todo-app/script.js
@@ -21,11 +21,13 @@ const addOrUpdateTask = () => {
     date: dateInput.value,
     description: descriptionInput.value,
   };
+
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   } else {
     taskData[dataArrIndex] = taskObj;
   }
+
   updateTaskContainer();
   reset();
 };
@@ -40,7 +42,7 @@ const updateTaskContainer = () => {
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
           <button onclick="editTask(this)" type="button" class="btn">Edit</button>
-          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `;
   });
@@ -50,6 +52,7 @@ const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
 };
@@ -58,11 +61,15 @@ const editTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
   currentTask = taskData[dataArrIndex];
+
   titleInput.value = currentTask.title;
   dateInput.value = currentTask.date;
   descriptionInput.value = currentTask.description;
+
   addOrUpdateTaskBtn.innerText = "Update Task";
+
   taskForm.classList.toggle("hidden");
 };
 
@@ -81,7 +88,12 @@ openTaskFormBtn.addEventListener("click", () =>
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues =
     titleInput.value || dateInput.value || descriptionInput.value;
-  if (formInputsContainValues) {
+  const formInputValuesUpdated =
+    titleInput.value !== currentTask.title ||
+    dateInput.value !== currentTask.date ||
+    descriptionInput.value !== currentTask.description;
+
+  if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
   } else {
     reset();
