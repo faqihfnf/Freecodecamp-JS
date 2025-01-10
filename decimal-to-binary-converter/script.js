cbd15227177f65465b25b6a09e8ce4b1684c7542
faqihfnf <faqih.fnf@gmail.com>
2025-01-10 16:15:24 +0700

    decimal-to-binary-converter until step 32

--- decimal-to-binary-converter/script.js
+++ decimal-to-binary-converter/script.js
@@ -6,6 +6,17 @@ function decimalToBinary(input) {
   const inputs = [];
   const quotients = [];
   const remainders = [];
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
 }
 
 const checkUserInput = () => {
