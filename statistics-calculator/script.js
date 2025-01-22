52a6e840cb6723e6eee4de99873c05dcb1d3d959
faqihfnf <faqih.fnf@gmail.com>
2025-01-22 18:17:50 +0700

    statistics-calculator until step 8

--- statistics-calculator/script.js
+++ statistics-calculator/script.js
@@ -1,1 +1,5 @@
-const calculate = () => {};
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+};
