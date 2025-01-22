0c4d127f68f0d67659461f327e4a1e118c2efff5
faqihfnf <faqih.fnf@gmail.com>
2025-01-22 20:05:09 +0700

    statistics-calculator until step 37

--- statistics-calculator/script.js
+++ statistics-calculator/script.js
@@ -1,5 +1,31 @@
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
+};
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => (counts[el] = counts[el] ? counts[el] + 1 : 1));
+  if (Set(Object.values(counts)).size === 0) {
+    return null;
+  }
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 };
