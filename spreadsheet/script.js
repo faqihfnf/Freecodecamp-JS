4f51c866795df684a1679bb592ba5aa2c166fd1b
faqihfnf <faqih.fnf@gmail.com>
2025-01-28 15:57:14 +0700

    spreadsheet until step 19

--- spreadsheet/script.js
+++ spreadsheet/script.js
@@ -2,6 +2,10 @@ const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
     .map((element, index) => element + index);
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -11,4 +15,16 @@ window.onload = () => {
     label.textContent = name;
     container.appendChild(label);
   };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+    });
+  });
 };
