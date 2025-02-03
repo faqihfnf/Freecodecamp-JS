6c444a5e89c6c9349c02ee0f78806bad34416e10
faqihfnf <faqih.fnf@gmail.com>
2025-02-03 10:30:07 +0700

    spreadsheet until step 63

--- spreadsheet/script.js
+++ spreadsheet/script.js
@@ -38,6 +38,10 @@ const evalFormula = (x, cells) => {
     (_match, char1, num1, char2, num2) =>
       rangeFromString(num1, num2).map(addCharacters(char1)(char2))
   );
+  const cellRegex = /[A-J][1-9][0-9]?/gi;
+  const cellExpanded = rangeExpanded.replace(cellRegex, (match) =>
+    idToText(match.toUpperCase())
+  );
 };
 
 window.onload = () => {
