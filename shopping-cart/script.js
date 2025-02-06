898ef9c67373a4f7de93f1535e300b1076495fab
faqihfnf <faqih.fnf@gmail.com>
2025-02-06 20:27:20 +0700

    shopping-cart until step 32

--- shopping-cart/script.js
+++ shopping-cart/script.js
@@ -116,9 +116,19 @@ class ShoppingCart {
       totalCountPerProduct[dessert.id] =
         (totalCountPerProduct[dessert.id] || 0) + 1;
     });
-    const currentProductCountSpan = document.getElementById(
-      `product-count-for-id${product.id}`
-    );
+
     const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(
+      `product-count-for-id${id}`
+    );
+
+    currentProductCount > 1
+      ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
+      : (productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+      <p><span class="product-count" id="product-count-for-id${id}"></span>${name}</p>
+      <p>${price}</p>
+      </div>
+      `);
   }
 }
