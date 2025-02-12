725d0cba0a8cd346fe273ba5e14fd35772f706b8
faqihfnf <faqih.fnf@gmail.com>
2025-02-12 10:31:30 +0700

    fcc-author-page step 28

--- fcc-author-page/script.js
+++ fcc-author-page/script.js
@@ -11,7 +11,9 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     authorDataArr = data;
     displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
   })
-  .catch((err) => console.error(`There was an error: ${err}`));
+  .catch((err) => {
+    authorContainer.innerHTML = `<p class="error-msg">There was an error loading the authors</p>`;
+  });
 
 const fetchMoreAuthors = () => {
   startingIndex += 8;
