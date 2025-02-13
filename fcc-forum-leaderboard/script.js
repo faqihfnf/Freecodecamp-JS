441181fecb5cfc48652e0bd040e6f385cd6190e0
faqihfnf <faqih.fnf@gmail.com>
2025-02-13 09:43:02 +0700

    fcc-forum-leaderboard step 4

--- fcc-forum-leaderboard/script.js
+++ fcc-forum-leaderboard/script.js
@@ -4,3 +4,5 @@ const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
+
+const fetchData = async () => {};
