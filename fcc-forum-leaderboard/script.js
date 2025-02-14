fef061d61c498578a453e1520dc8a0f7890eec6b
faqihfnf <faqih.fnf@gmail.com>
2025-02-14 13:28:31 +0700

    fcc-forum-leaderboard step 40

--- fcc-forum-leaderboard/script.js
+++ fcc-forum-leaderboard/script.js
@@ -33,6 +33,7 @@ const forumCategory = (id) => {
   }
   const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
   const linkText = selectedCategory.category;
+  const linkClass = `category ${selectedCategory.className}`;
 };
 
 const timeAgo = (time) => {
