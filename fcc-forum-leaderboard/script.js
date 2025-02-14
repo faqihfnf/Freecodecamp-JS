c77ed7d87ec4d3b9d6ffe7ecc22de01ee4b66ba3
faqihfnf <faqih.fnf@gmail.com>
2025-02-14 09:52:58 +0700

    fcc-forum-leaderboard step 22

--- fcc-forum-leaderboard/script.js
+++ fcc-forum-leaderboard/script.js
@@ -4,6 +4,10 @@ const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+};
 
 const fetchData = async () => {
   try {
