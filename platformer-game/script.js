c7405b245ddddff77b408c755fc7cfbd8b452bdc
faqihfnf <faqih.fnf@gmail.com>
2025-02-17 20:39:44 +0700

    platformer-game step 28

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -36,6 +36,7 @@ class Player {
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       if (this.position.y < 0) {
         this.position.y = 0;
+        this.velocity.y = gravity;
       }
     }
   }
