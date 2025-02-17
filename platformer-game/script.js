a28eedb9f17cc810be1f52ca408a104e05c801e7
faqihfnf <faqih.fnf@gmail.com>
2025-02-17 19:20:33 +0700

    platformer-game step 11

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -8,4 +8,6 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.5;
 let isCheckpointCollisionDetectionActive = true;
-const proportionalSize = (size) => {};
+const proportionalSize = (size) => {
+  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+};
