a567a571bb5ddf702bdd2984d28ef4ea86402dce
faqihfnf <faqih.fnf@gmail.com>
2025-02-23 01:34:38 +0700

    platformer-game step 103

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -135,6 +135,9 @@ const animate = () => {
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x -= 5;
+      });
     } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
       platforms.forEach((platform) => {
         platform.position.x += 5;
