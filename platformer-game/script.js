0473b4d91daa73ef82ba3eeeecb03005531fb5f3
faqihfnf <faqih.fnf@gmail.com>
2025-02-23 01:12:38 +0700

    platformer-game step 95

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -67,7 +67,12 @@ class Platform {
 }
 
 class CheckPoint {
-  constructor(x, y, z) {}
+  constructor(x, y, z) {
+    this.position = {
+      x,
+      y,
+    };
+  }
 }
 
 const player = new Player();
