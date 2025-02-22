cfd613472170da0576d695578799d8712f5762ab
faqihfnf <faqih.fnf@gmail.com>
2025-02-23 01:36:26 +0700

    platformer-game step 104

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -142,6 +142,9 @@ const animate = () => {
       platforms.forEach((platform) => {
         platform.position.x += 5;
       });
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x += 5;
+      });
     }
   }
 
